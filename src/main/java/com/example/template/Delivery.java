1c87e72f5a134612834ef3f0e3e885b60478418b
kimscott <sanaloveyou@uengine.org>
2020-02-14 16:58:02 +0900

    first commit

---
+++ src/main/java/com/example/template/Delivery.java
@@ -0,0 +1,87 @@
+package com.example.template;
+
+public class Delivery {
+
+    private Long deliveryId;
+    private Long orderId;
+    private int quantity;
+    private Long productId;
+    private String productName;
+    private String customerId;
+    private String customerName;
+    private String deliveryAddress;
+    private String deliveryState;
+
+    public Long getDeliveryId() {
+        return deliveryId;
+    }
+
+    public void setDeliveryId(Long deliveryId) {
+        this.deliveryId = deliveryId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getDeliveryState() {
+        return deliveryState;
+    }
+
+    public void setDeliveryState(String deliveryState) {
+        this.deliveryState = deliveryState;
+    }
+
+}
---
+++ src/main/java/com/example/template/DeliveryService.java
@@ -0,0 +1,21 @@
+package com.example.template;
+
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@Service
+@FeignClient(name ="delivery", url="http://localhost:8082")
+public interface DeliveryService {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/deliveries", consumes = "application/json")
+    void startDelivery(Delivery delivery);
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/deliveries/{deliveryId}", consumes = "application/json")
+    void completeDelivery(@PathVariable("deliveryId") Long deliveryId, Delivery delivery);
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/deliveries/{deliveryId}", consumes = "application/json")
+    void cancelDelivery(@PathVariable("deliveryId") Long deliveryId,  Delivery delivery);
+}
